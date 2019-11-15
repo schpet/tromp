7eb56c97b73324ab8d4b55e77b441f7eb507f849
Peter Schilling <peter@peterschilling.org>
2019-11-15 14:31:10 -0800

    Basics & cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -1,34 +1,94 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode"
+import * as alt from "alternate-file"
+import { relative } from "path"
+import { promises as fs } from "fs"
+import * as path from "path"
+import * as t from "io-ts"
+import { isLeft } from "fp-ts/lib/Either"
+import * as minimatch from "minimatch"
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const TrompTests = t.interface({
+  match: t.string,
+  command: t.string,
+})
+
+const TrompConfig = t.interface({
+  tests: t.array(TrompTests),
+})
+
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "tromp" is now active!')
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
-    "extension.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
+    "tromp.runTest",
+    async () => {
+      const { workspaceFolders } = vscode.workspace
+      const root = workspaceFolders && workspaceFolders[0]
+      if (!root) {
+        vscode.window.showErrorMessage("no workspace")
+        return
+      }
+
+      let trompConfig
+      try {
+        const configPath = path.join(root.uri.fsPath, "tromp.json")
+        const configFile = await fs.readFile(configPath)
+        const configData = JSON.parse(configFile.toString())
+        const result = TrompConfig.decode(configData)
+
+        if (isLeft(result)) {
+          const validationMessage = result.left.map(x => x.message)
+          throw new Error(`validation error: ${validationMessage}`)
+        }
+
+        trompConfig = result.right
+      } catch (e) {
+        vscode.window.showErrorMessage(`Problem with tromp.json: ${e.message}`)
+        return
+      }
+
       const editor = vscode.window.activeTextEditor
       if (!editor) {
-        vscode.window.showErrorMessage("no editor :(")
-        return
+        return vscode.window.showErrorMessage("no editor :(")
+      }
+
+      const { fsPath } = editor.document.uri
+      let relativeTestPath = relative(
+        root.uri.fsPath,
+        editor.document.uri.fsPath
+      )
+
+      let command = trompConfig.tests.find(configTestEntry =>
+        minimatch(relativeTestPath, configTestEntry.match)
+      )
+
+      // file isn't a match.
+      // use projections.json to see if an alternative matches
+      if (!command) {
+        const alternate = await alt.findAlternateFile(fsPath)
+        if (!alt.isAlternateFileNotFoundError(alternate)) {
+          // @ts-ignore
+          const altFsPath: string = alternate.ok
+          relativeTestPath = relative(root.uri.fsPath, altFsPath)
+
+          command = trompConfig.tests.find(configTestEntry =>
+            minimatch(relativeTestPath, configTestEntry.match)
+          )
+        }
+      }
+
+      if (!command) {
+        return vscode.window.showErrorMessage(
+          `tromp.json has no match for ${relativeTestPath}`
+        )
       }
 
       const terminal =
         vscode.window.activeTerminal || vscode.window.createTerminal("tromp")
 
       terminal.show(true)
-
-      const { fileName } = editor.document
-      terminal.sendText(`yarn test ${fileName}`)
+      await vscode.commands.executeCommand("workbench.action.terminal.clear")
+      terminal.sendText(`${command.command} ${relativeTestPath}`)
     }
   )
 
