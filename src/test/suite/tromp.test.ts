9dc39c31469c0004f61439a5c0ecde79f0411050
Peter Schilling <peter@peterschilling.org>
2019-12-16 21:56:01 -0800

    Test only the happy path

--- src/test/suite/tromp.test.ts
+++ src/test/suite/tromp.test.ts
@@ -8,20 +8,20 @@ suite("Extension Test Suite", () => {
   vscode.window.showInformationMessage("Start all tests.")
 
   test("it runs a command successfully", async () => {
-    const errorMessageSpy = sinon.spy(vscode.window, "showErrorMessage")
-
     const { workspaceFolders = [] } = vscode.workspace
     assert.equal(workspaceFolders.length, 1, "one workspace is open")
     const workspace = workspaceFolders[0]
 
-    await vscode.commands.executeCommand("tromp.runCommandWithFile")
+    // const errorMessageSpy = sinon.spy(vscode.window, "showErrorMessage")
 
-    assert.ok(errorMessageSpy.called, "an error message is shown")
-    assert.equal(
-      errorMessageSpy.getCall(0).args[0],
-      "Tromp needs an editor to run",
-      "shows editor error message"
-    )
+    // await vscode.commands.executeCommand("tromp.runCommandWithFile")
+
+    // assert.ok(errorMessageSpy.called, "an error message is shown")
+    // assert.equal(
+    //   errorMessageSpy.getCall(0).args[0],
+    //   "Tromp needs an editor to run",
+    //   "shows editor error message"
+    // )
 
     await vscode.window.showTextDocument(
       vscode.Uri.file(path.join(workspace.uri.fsPath, "cool.txt"))
