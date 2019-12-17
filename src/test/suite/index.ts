9777e7948dcc80d38e4ba47cf3abf69617bf69bc
Peter Schilling <peter@peterschilling.org>
2019-12-16 21:41:32 -0800

    Test happy path

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -7,6 +7,7 @@ export function run(): Promise<void> {
   // Create the mocha test
   const mocha = new Mocha({
     ui: "tdd",
+    timeout: 15 * 1000,
   })
   mocha.useColors(true)
 
---
+++ src/test/suite/tromp.test.ts
@@ -0,0 +1,44 @@
+import * as assert from "assert"
+import * as vscode from "vscode"
+import * as path from "path"
+import * as sinon from "sinon"
+import * as util from "../../util"
+
+const sleep = (m: number) => new Promise(r => setTimeout(r, m))
+
+suite("Extension Test Suite", () => {
+  vscode.window.showInformationMessage("Start all tests.")
+
+  test("it runs a command successfully", async () => {
+    const errorMessageSpy = sinon.spy(vscode.window, "showErrorMessage")
+
+    const { workspaceFolders = [] } = vscode.workspace
+    assert.equal(workspaceFolders.length, 1, "one workspace is open")
+    const workspace = workspaceFolders[0]
+
+    await vscode.commands.executeCommand("tromp.runCommandWithFile")
+
+    assert.ok(errorMessageSpy.called, "an error message is shown")
+    assert.equal(
+      errorMessageSpy.getCall(0).args[0],
+      "Tromp needs an editor to run",
+      "shows editor error message"
+    )
+
+    await vscode.window.showTextDocument(
+      vscode.Uri.file(path.join(workspace.uri.fsPath, "cool.txt"))
+    )
+
+    const terminal = util.trompTerminal()
+
+    return new Promise(resolve => {
+      sinon.replace(terminal, "sendText", text => {
+        assert.equal(text, "cat cool.txt")
+        sinon.restore()
+        resolve()
+      })
+
+      vscode.commands.executeCommand("tromp.runCommandWithFile")
+    })
+  })
+})
